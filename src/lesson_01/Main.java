d2f51ed2f9e385b9a61c8ffcca7530a0983ede21
Yevhen Kim <evgeniikim@gmail.com>
2023-09-06 06:18:53 +0200

    + lesson 1

---
+++ src/lesson_01/Main.java
@@ -0,0 +1,12 @@
+//-------------------------------------------------
+//	 Copyright (C) 2023
+//	 Email:    evgeniikim@gmail.com
+//   Author:   Yevhen Kim
+//-------------------------------------------------
+package lesson_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
