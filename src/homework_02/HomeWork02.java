1d6bf3563a63292ae88f0de8496211995adc80fe
Yevhen Kim <evgeniikim@gmail.com>
2023-09-06 06:19:30 +0200

    + homework 2

---
+++ src/homework_02/HomeWork02.java
@@ -0,0 +1,20 @@
+//-------------------------------------------------
+//	 Copyright (C) 2023
+//	 Email:    evgeniikim@gmail.com
+//   Author:   Yevhen Kim
+//-------------------------------------------------
+package homework_02;
+
+public class HomeWork02 {
+
+    public static String FirstName = "Yevhen";
+    public static byte Age = 44;
+    public static double Price_Milk = 1.99;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // # < > + - , . : ; ( ) { } / \
+
+    }
+}
