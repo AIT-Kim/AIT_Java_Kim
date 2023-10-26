19ae71606db6fdcbb3125d60540b03b061f10e26
Yevhen Kim <evgeniikim@gmail.com>
2023-10-26 19:16:06 +0200

    + lesson_37

---
+++ src/lesson_37/testComparators/Task1Comparator.java
@@ -0,0 +1,20 @@
+package lesson_37.testComparators;
+
+import java.util.Comparator;
+
+public class Task1Comparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer int1, Integer int2) {
+        boolean isFirstEven = (int1 % 2) == 0;
+        boolean isSecondEven = (int2 % 2) == 0;
+
+        if (isFirstEven && !isSecondEven) { // первое четное, второе не четное -> первое должно быть "меньше" (т.е. располагаться левее)
+            return -1;
+        } else if (!isFirstEven && isSecondEven) { // второе четное, первое не четное -> первое должно быть "больше" (т.е. располагаться правее)
+            return 1;
+        } else { // оба либо четных, либо не четных - сортируем в порядке возрастания
+            return Integer.compare(int1, int2);
+        }
+
+    }
+}
