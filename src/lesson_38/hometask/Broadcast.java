23b79c88fd5e17ed2431f89cf7a8ebcad9500fd5
Yevhen Kim <evgeniikim@gmail.com>
2023-10-27 20:32:45 +0200

    +

---
+++ src/lesson_38/hometask/Broadcast.java
@@ -0,0 +1,14 @@
+package lesson_38.hometask;
+
+public class Broadcast {
+
+    private String title; // название нашей передачи
+
+    public Broadcast(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
