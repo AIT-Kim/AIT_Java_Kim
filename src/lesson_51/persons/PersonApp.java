5e695cbf492431c9dabb45b21f3fbe964dba4b58
Yevhen Kim <evgeniikim@gmail.com>
2023-11-16 19:15:27 +0100

    + lesson_51

---
+++ src/lesson_51/persons/PersonApp.java
@@ -0,0 +1,15 @@
+package lesson_51.persons;
+
+public class PersonApp {
+    public static void main(String[] args) {
+
+        Person person = new Person("john@email.com", "qwertY1!");
+        System.out.println(person + " : " + person.getPassword());
+
+        Person person2 = new Person("john@ema%il.com", "qwertyU1");
+        System.out.println(person2 + " : " + person2.getPassword());
+
+        Person person3 = new Person("john@email.com", "QWERTY1!");
+        System.out.println(person3 + " : " + person3.getPassword());
+    }
+}
