4df1077d338156c0d04ef10036149b9f61d8ed23
Yevhen Kim <evgeniikim@gmail.com>
2023-12-19 20:24:17 +0100

    + lesson_68

---
+++ src/lesson_68/generator/GeneratorAtomic.java
@@ -0,0 +1,16 @@
+package lesson_68.generator;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GeneratorAtomic {
+    private final AtomicInteger value = new AtomicInteger();
+    private static final int DELTA = 2;
+
+    public int generate() {
+        return this.value.getAndAdd(DELTA); // атомарно прибавляет дельту и возвращает старое значение
+    }
+
+    public int getValue() {
+        return this.value.intValue(); // возвращает целочисленное значение
+    }
+}
